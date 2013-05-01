ad98caf915628652e3cbf2b2db799e680db3120e
Vinícius Kirst <vinicius.kirst@gmail.com>
2013-05-01 20:01:10 -0300

    number picker

--- gen/com/kirst/vinicius/flex/R.java
+++ gen/com/kirst/vinicius/flex/R.java
@@ -24,16 +24,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f08000a;
         public static final int button1=0x7f080003;
         public static final int editTextAlcohol=0x7f080004;
         public static final int editTextGas=0x7f080001;
+        public static final int numberPicker1=0x7f080006;
+        public static final int numberPicker2=0x7f080007;
+        public static final int numberPicker3=0x7f080008;
+        public static final int numberPicker4=0x7f080009;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080002;
         public static final int textViewResult=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int gas_price_dialog=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -44,6 +49,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int calculate=0x7f050005;
         public static final int cancel=0x7f050008;
+        public static final int coma=0x7f05000b;
         public static final int dialogGasPriceTitle=0x7f05000a;
         public static final int empty=0x7f050007;
         public static final int gas=0x7f050003;
